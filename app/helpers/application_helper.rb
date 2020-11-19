4b31d05a5a366fc978913c395bbc29993e257403
Diego Salazar <mastermindxs@gmail.com>
2020-11-19 16:05:11 -0500

    added bootstrap icons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def icon(icon, options = {})
+    file = File.read "node_modules/bootstrap-icons/icons/#{icon}.svg"
+    doc = Nokogiri::HTML::DocumentFragment.parse file
+    svg = doc.at_css 'svg'
+
+    options.each_pair { |key, val| svg[key] = val }
+
+    doc.to_html.html_safe
+  end
 end
