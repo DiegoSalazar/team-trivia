4e42ca208e648808d90868194ca3e2599f02e887
Diego Salazar <mastermindxs@gmail.com>
2021-01-31 22:07:53 -0500

    wrote vote_component spec

--- spec/components/vote_component_spec.rb
+++ spec/components/vote_component_spec.rb
@@ -1,13 +1,28 @@
-require "rails_helper"
+# frozen_string_literal: true
+
+require 'rails_helper'
 
 RSpec.describe VoteComponent, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { described_class.new player, guess }
+  before { guess.liked_by player }
+  let(:player) { create :player }
+  let(:guess) { create :guess }
 
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
-  # end
+  describe '#link_class' do
+    it 'is a string' do
+      expect(subject.link_class).to be_a String
+    end
+
+    it 'includes active when the player voted' do
+      expect(subject.link_class).to include 'active'
+    end
+
+    context 'player did not vote' do
+      before { Vote.destroy_all }
+
+      it 'has border and shadow' do
+        expect(subject.link_class).to include 'border shadow'
+      end
+    end
+  end
 end
