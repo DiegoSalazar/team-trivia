e329bcfd06387a81205b5e33ec872a54ef433329
Diego Salazar <mastermindxs@gmail.com>
2021-01-31 21:40:48 -0500

    wrote trivum banner component spec

--- spec/components/trivium_banner_component_spec.rb
+++ spec/components/trivium_banner_component_spec.rb
@@ -1,13 +1,22 @@
-require "rails_helper"
+# frozen_string_literal: true
+
+require 'rails_helper'
 
 RSpec.describe TriviumBannerComponent, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { described_class.new trivium }
+  let(:trivium) { create :trivium }
 
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
-  # end
+  it 'renders the trivium body' do
+    expect(render_inline(subject).to_html).to include trivium.body
+  end
+
+  describe '#starts_at' do
+    it 'is a string' do
+      expect(subject.starts_at).to be_a String
+    end
+
+    it 'displays the trivium start time' do
+      expect(subject.starts_at).to eq I18n.l(trivium.game_starts_at, format: :short)
+    end
+  end
 end
