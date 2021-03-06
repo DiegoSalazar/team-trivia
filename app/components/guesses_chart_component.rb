52deb2029ddf953ccf993f2bd863a496237c91c6
Diego Salazar <mastermindxs@gmail.com>
2021-03-06 15:41:30 -0500

    showing the guess value in the middle and percent of similiar guesses on top of the bar

--- app/components/guesses_chart_component.rb
+++ app/components/guesses_chart_component.rb
@@ -4,5 +4,14 @@ class GuessesChartComponent < ViewComponent::Base
   def initialize(question)
     super
     @question = question
+    @guesses_count = question.guesses.count
+  end
+
+  def percent_of_similar_guesses(guess)
+    number_to_percentage guess.same_count_percent_of(@guesses_count), precision: 1
+  end
+
+  def value_for_middle_unit(guess, i)
+    guess.value if i == guess.similarity_ratio / 2
   end
 end
