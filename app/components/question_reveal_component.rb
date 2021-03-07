d738614b55a1d0e9ac6b541e01c6a5764b3542e6
Diego Salazar <mastermindxs@gmail.com>
2021-03-06 20:12:19 -0500

    Passing args for reveal status styles

--- app/components/question_reveal_component.rb
+++ app/components/question_reveal_component.rb
@@ -1,9 +1,22 @@
 # frozen_string_literal: true
 
 class QuestionRevealComponent < ViewComponent::Base
-  def initialize(question, question_index)
+  def initialize(question, question_index, reveal_status, current_question_revealed)
     super
     @question = question
     @question_index = question_index
+    @reveal_status = reveal_status
+    @current_question_revealed = current_question_revealed
+  end
+
+  def reveal_status(question)
+    status = []
+    status << 'revealed' if @reveal_status[question.id]
+    status << 'active' if active? question
+    status.join ' '
+  end
+
+  def active?(question)
+    @current_question_revealed == question.id
   end
 end
