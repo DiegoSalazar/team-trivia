c32d80c8fc2483f5e498a07d49ab9162f1c2d6fc
Diego Salazar <mastermindxs@gmail.com>
2021-03-06 20:15:34 -0500

    Implement the QuestionTemplate#reveal reflex action

--- app/reflexes/question_template_reflex.rb
+++ app/reflexes/question_template_reflex.rb
@@ -11,6 +11,33 @@ class QuestionTemplateReflex < ApplicationReflex
     morph :nothing
   end
 
+  def reveal
+    @reveal_status = session[:reveal_status]
+    @current_question_revealed = session[:current_question_revealed]
+    next_question_id, _ = @reveal_status.detect { |_, v| !v }
+    return if next_question_id.nil?
+
+    @reveal_status[next_question_id] = true
+    session[:reveal_status] = @reveal_status
+    session[:current_question_revealed] = next_question_id
+
+    # Reveal the next question
+    %w[revealed active].each do |klass|
+      cable_ready['trivium_reveal'].add_css_class \
+        selector: "#question_template_#{next_question_id}",
+        name: klass
+    end
+
+    if @current_question_revealed
+      cable_ready['trivium_reveal'].remove_css_class \
+        selector: "#question_template_#{@current_question_revealed}",
+        name: 'active'
+    end
+
+    cable_ready.broadcast
+    morph :nothing
+  end
+
   def cancel
     @question_template = nil
   end
