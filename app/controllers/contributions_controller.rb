15ad71b468610da3cee34740e4c34275b35af3f7
Diego Salazar <mastermindxs@gmail.com>
2021-03-12 01:29:29 -0500

    setting current_trivium in contributions#new

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -1,5 +1,6 @@
 class ContributionsController < ApplicationController
   before_action :authenticate_player!
+  before_action :set_current_trivium
 
   def new
     @upcoming_trivia = Trivium.upcoming
