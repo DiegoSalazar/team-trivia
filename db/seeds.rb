2e5b16b1744e2a412271cfd5f203cda4a630044c
Diego Salazar <mastermindxs@gmail.com>
2021-03-13 20:39:15 -0500

    Fixed seeds file, updating trivia times at the end

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #     s=START_OFFSET_IN_MINS
 #     e=END_OFFSET_IN_MINS
 
-def create_realistic_trivium(player, questions, team_count, guess_count, starts_at, ends_at, i)
+def create_realistic_trivium(player, questions, team_count, guess_count, i)
   puts "\nCreating Trivia #{i.succ}"
   trivium = FactoryBot.create \
     :trivium,
@@ -15,7 +15,7 @@ def create_realistic_trivium(player, questions, team_count, guess_count, starts_
     body: 'A mysterious hint'
 
   if i.zero? # only create teams once
-    print "\nCreating Teams" if team_count.positive?
+    print "\nCreating #{team_count} Teams" if team_count.positive?
     FactoryBot.create_list :team, team_count, :with_players
   end
 
@@ -36,20 +36,23 @@ def create_realistic_trivium(player, questions, team_count, guess_count, starts_
 
   Team.all.each_with_index do |team, t|
     print "\n#{i.succ}: Team #{t} -> "
-    team.players.each_with_index do |player, p|
-      print "Player #{p}"
+
+    team.players.each_with_index do |plyr, p|
       if rand > 0.5
+        print "Player #{p}"
         FactoryBot.create \
           :team_message,
-          player: player,
+          player: plyr,
           team: team,
           trivium: trivium
         print ?.
       end
+      print ' '
 
       guess_count.times do
-        trivium.questions.each do |question|
+        trivium.questions.each_with_index do |question, q|
           next if rand > 0.1
+          print "Q#{q}"
 
           FactoryBot.create \
             :guess_message,
@@ -60,33 +63,31 @@ def create_realistic_trivium(player, questions, team_count, guess_count, starts_
             guess: create_guess_for(player, question.reload, team, trivium)
           print ?.
         end
+        print ' '
       end
-      print ' '
     end
 
     print "\n#{i.succ}: Voting Team #{t}"
-    team.players.each do |player|
+    team.players.each do |p|
       next if rand > 0.3
 
-      team.guesses.each do |guess|
+      team.guesses_for(trivium).each do |guess|
         next if rand > 0.1
 
-        guess.vote_by voter: player
+        guess.vote_by voter: p
         print ?.
       end
     end
     puts
   end
 
-  trivium.game_starts_at = starts_at
-  trivium.game_ends_at = ends_at
-  trivium.save!
+  trivium
 end
 
 def create_guess_for(player, question, team, trivium)
   attributes = { player: player, question: question, team: team, trivium: trivium, cached_votes_up: 0 }
   attributes[:value] = question.answers.first.value if rand > 0.3
-  attributes[:value] = question.guesses.sample&.value if rand > 0.4
+  attributes[:value] = question.guesses.sample&.value if rand > 0.3
   attributes.delete :value if attributes[:value].nil?
   FactoryBot.create :guess, attributes
 end
@@ -108,9 +109,6 @@ ActiveRecord::Base.transaction do
     player2.teams << team
   end
 
-  starts_at = 40.seconds.from_now
-  starts_at = ENV['s'].to_i.seconds.from_now if ENV['s'].present?
-  ends_at = starts_at + (ENV['e'] || 80).to_i.seconds
   trivia_count = (ENV['n'] || 1).to_i
   team_count = (ENV['t'] || 3).to_i
   guess_count = (ENV['g'] || 3).to_i
@@ -120,17 +118,27 @@ ActiveRecord::Base.transaction do
 
   print "\nCreating #{trivia_count} Trivia"
 
-  trivia_count.times do |i|
-    create_realistic_trivium \
+  trivia = trivia_count.times.map do |i|
+    t = create_realistic_trivium \
       player,
       questions,
       team_count,
       guess_count,
-      starts_at,
-      ends_at,
       i
     print ?.
+    t
   end
+
+  trivia.each_with_index do |trivium, i|
+    starts_at = Time.now
+    starts_at = (ENV['s'].to_i + i * 60).seconds.from_now if ENV['s'].present?
+    ends_at = starts_at + (ENV['e'] || 80).to_i.seconds
+
+    trivium.game_starts_at = starts_at
+    trivium.game_ends_at = ends_at
+  end
+
+  trivia.each &:save!
   puts "\nDone."
 
 rescue RuntimeError => e
