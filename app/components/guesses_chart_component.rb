9b419668c9e8f362b5b14a62784c5165eb58e9eb
Diego Salazar <mastermindxs@gmail.com>
2021-03-13 19:45:22 -0500

    Improve guessesChartComponent bar chart, use height percentage

--- app/components/guesses_chart_component.rb
+++ app/components/guesses_chart_component.rb
@@ -8,11 +8,13 @@ class GuessesChartComponent < ViewComponent::Base
   end
 
   def percent_of_similar_guesses(guess)
+    return '&nbsp;'.html_safe if guess.is_a? Question::CorrectAnswer
+
     number_to_percentage guess.same_count_percent_of(@guesses_count), precision: 1
   end
 
-  def value_for_middle_unit(guess, i)
-    guess.value if i == guess.similarity_ratio / 2
+  def height_percent(guess)
+    "height: #{guess.similarity_ratio}%;"
   end
 
   def answer_status(guess)
