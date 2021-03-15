69071e58d3570d9669b80d4ad8a8263257e50a2e
Diego Salazar <mastermindxs@gmail.com>
2021-03-14 22:07:06 -0400

    add dotenv load to rspec rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,14 +1,23 @@
+# frozen_string_literal: true
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+
+require 'dotenv'
+Dotenv.load('.env')
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
+
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort 'The Rails environment is running in production mode!' if Rails.env.production?
 require 'rspec/rails'
+
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'support/features/login'
 
 RSpec.configure do |config|
-  # config.include Devise::TestHelpers, :type => :request
+  # config.include Devise::TestHelpers, type: :controller
+  config.include Support::Features::Login, type: :feature
 end
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -34,6 +43,7 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
   config.include ViewComponent::TestHelpers, type: :component
