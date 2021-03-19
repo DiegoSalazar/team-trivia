4305d617e5ec53dbdf1041b5eae80ee42bd14b7e
Diego Salazar <mastermindxs@gmail.com>
2021-03-18 22:22:49 -0400

    replaced redis with db based sessions

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,11 +1,3 @@
 # frozen_string_literal: true
 
-Rails.application.config.session_store :redis_session_store, {
-  key: "_#{Rails.application.class.module_parent_name.downcase}_session",
-  redis: {
-    expire_after: 8.hours,  # cookie expiration
-    ttl: 8.hours,           # Redis expiration, defaults to 'expire_after'
-    key_prefix: 'tt:session:',
-    url: ENV['REDIS_URL'] || 'redis://localhost:6379/0'
-  }
-}
+Rails.application.config.session_store :active_record_store, key: '_team_trivia_session'
