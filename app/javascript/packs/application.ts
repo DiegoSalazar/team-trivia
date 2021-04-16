2a4c50fd43d70c08e5f88a59f96aabbb4437ca54
Diego Salazar <mastermindxs@gmail.com>
2021-04-16 16:34:45 -0400

    added datetimepicker

--- app/javascript/packs/application.ts
+++ app/javascript/packs/application.ts
@@ -2,9 +2,22 @@ require('@rails/ujs').start()
 require('turbolinks').start()
 // require('@rails/activestorage').start()
 require('channels')
-// require('controllers')
-import 'bootstrap'
-import 'controllers'
+require('controllers')
+const $ = require('jquery')
+require('bootstrap')
+require('bootstrap4-datetimepicker')
+
+const isChrome = /chrome/i.test(navigator.userAgent)
+
+document.addEventListener('load', onLoad)
+document.addEventListener('turbolinks:load', onLoad)
+
+function onLoad () {
+  if (!isChrome) {
+    // Chrome has its own datetime picker built in
+    $('[type^="datetime"]').datetimepicker()
+  }
+}
 
 // Bootstraps alerts were not working. They weren't showing on their own.
 // Had to add CSS in application.scss to force them to show
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,20 @@
+const path = require('path')
+const { ProvidePlugin } = require('webpack')
 const { environment } = require('@rails/webpacker')
 const typescript =  require('./loaders/typescript')
 
 environment.loaders.prepend('typescript', typescript)
+
+environment.plugins.prepend('Provide',
+  new ProvidePlugin({
+    $: 'jquery/src/jquery',
+    datetimepicker: 'bootstrap4-datetimepicker/src/js/datetimepicker'
+  })
+)
+
+environment.config.resolve.alias = {
+  // Force all modules to use the same jquery version
+  'jquery': 'jquery/src/jquery'
+}
+
 module.exports = environment
